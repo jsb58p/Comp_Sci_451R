fa32261eca7e9e2144727e53ff3a5dbf7446e95d
KZhangCodes <kzkm8@umsystem.edu>
2026-03-06 01:00:59 -0600

    forgot password feature, sends email with password reset link

--- server/src/main/java/com/example/server/config/CorsConfig.java
+++ server/src/main/java/com/example/server/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "http://192.168.1.202:5173")
+                        .allowedOriginPatterns("http://localhost:*", "http://192.168.*.*:*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
